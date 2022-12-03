53523f3478cfc85b564c291ef7e41c112b119cf7
Kirill Kaiumov <k.kaiumov@catawiki.nl>
2022-12-03 11:12:02 +0100

    Day 3

--- lib/day3/b.ex
+++ lib/day3/b.ex
@@ -9,26 +9,15 @@ defmodule Day3.B do
     res =
       input
       |> split("\n")
-      |> find_common_char_in_groups()
+      |> chunk_every(3)
+      |> map(&find_common_char/1)
       |> map(&get_priority/1)
       |> sum()
 
     puts(res)
   end
 
-  def find_common_char_in_groups(lines) do
-    groups_count = div(length(lines), 3)
-
-    for group_index <- 0..(groups_count - 1) do
-      line1 = at(lines, group_index * 3)
-      line2 = at(lines, group_index * 3 + 1)
-      line3 = at(lines, group_index * 3 + 2)
-
-      find_common_char(line1, line2, line3)
-    end
-  end
-
-  def find_common_char(line1, line2, line3) do
+  def find_common_char([line1, line2, line3]) do
     part1 = MapSet.new(String.to_charlist(line1))
     part2 = MapSet.new(String.to_charlist(line2))
     part3 = MapSet.new(String.to_charlist(line3))
